e4a0f8ba43d5c3d20bf76b6417ba2f9dd3c04575
FG98 <81941690+FG98F@users.noreply.github.com>
2022-06-18 10:08:33 -0400

    Update fun-gay.js

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let name = conn.getName(who)
   conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/gay', {
-    avatar: await conn.profilePictureUrl(who).catch(_ => 'https://i.ibb.co/PZNv21q/Profile-FG98.jpg'),
+    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png'), 
   }), 'gay.png', `🏳️‍🌈  *Gay :* ${name}\n\nQuién quiere violar a este gay? `, m)
 }
 
