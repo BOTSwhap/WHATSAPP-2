b38be1485ad36a144c53895d1b4b39543fa53dc1
FG98 <81941690+FG98F@users.noreply.github.com>
2022-06-23 10:57:21 -0400

    Update gp-setwelcome.js

--- plugins/gp-setwelcome.js
+++ plugins/gp-setwelcome.js
@@ -1,9 +1,7 @@
+import db from '../lib/database.js'
+
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
-    if (isROwner) global.conn.welcome = text
-    
-   //else if (isOwner) conn.welcome = text
-    else if (isOwner) global.conn.welcome = text
     db.data.chats[m.chat].sWelcome = text
     m.reply('✅ Se configuro el mensaje de bienvenida')
   } else throw `✳️ Ingrese el mensaje de Bienvenida\n\n@user (mención)\n@group (Nombre de grupo)\n@desc (description de grupo)`
